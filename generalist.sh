84affcb2b4f8648c75b2220bab5b3e9ab4af39e3
Carlo Miguel F. Cruz <carloc@balena.io>
2022-01-03 10:26:41 +0800

    Update the tools to match the role.
    Change-type: minor
    Signed-off-by: Carlo Miguel F. Cruz <carloc@balena.io>

--- generalist.sh
+++ generalist.sh
@@ -3,11 +3,11 @@
 # Install Homebrew
 test -f /usr/local/bin/brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Install iterm2
+brew install --cask iterm2
+
 # Change shell to Z Shell
 chsh -s /bin/zsh
-
-# Install git flow
-brew install git-flow
 
 # Install Zsh Autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
@@ -25,19 +25,25 @@ cd fonts
 cd ..
 rm -rf fonts
 
+# Install Github CLI
+brew install gh
+
+# Install cuelang
+brew install cue-lang/tap/cue
+
 # Install Pyenv
 brew install pyenv
 grep 'export PYENV_ROOT="$HOME/.pyenv"' ~/.zshrc || echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 grep 'export PATH="$PYENV_ROOT/bin:$PATH"' ~/.zshrc || echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 grep 'eval "$(pyenv init -)"' ~/.zshrc || echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
 eval "$(pyenv init -)"
-pyenv install 3.7.6
+pyenv install 3.8.12
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 # Install AWS CLI
-pip3 install awscli
-
-# Install aws-iam-authenticator for EKS
-brew install aws-iam-authenticator
+brew install awscli
 
 # Install GPG
 brew install gnupg
@@ -46,45 +52,30 @@ grep 'export GPG_TTY=$(tty)' ~/.zshrc || echo 'export GPG_TTY=$(tty)' >> ~/.zshr
 # Install Terraform
 brew install terraform
 
+# Install Terragrunt
+brew install terragrunt
+
 # Install Kubernetes CLI
 brew install kubectl
 
-# Install Help
-brew install helm
-
 # Install Flux CLI
-brew install fluxctl
+brew install fluxcd/tap/flux
 
 # Install K9s CLI
 brew install derailed/k9s/k9s
 
-# Install Hyperkit
-brew install hyperkit
-
-# Install Minikube
-brew install minikube
-
-# Install Docker CLI
-brew install docker
-
-# Install Virtualbox
-brew cask install virtualbox
-
-# Install Vagrant
-brew cask install vagrant
-
 # Install Visual Studio Code
-brew cask install visual-studio-code
+brew install --cask visual-studio-code
 
 # Install Google Chrome
-brew cask install google-chrome
+brew install --cask google-chrome
 
-# Install KeepassXC
-brew cask install keepassxc
+# Install Docker Desktop
+brew install --cask docker
 
 # Update existing packages
 brew update
 brew upgrade
-brew cask upgrade
+brew upgrade --cask
 
 echo 'Done.'
