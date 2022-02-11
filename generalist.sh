3f723031a859738b8b6f25f78a7e5ba2c9aef476
Carlo Miguel F. Cruz <carloc@balena.io>
2022-02-11 12:04:02 +0000

    Consolidate package installations. Require git commit signing.
    Change-type: patch
    Signed-off-by: Carlo Miguel F. Cruz <carloc@balena.io>

--- generalist.sh
+++ generalist.sh
@@ -1,38 +1,53 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 # Install Homebrew
 test -f /usr/local/bin/brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install iterm2
-brew install --cask iterm2
+# Install packages using Homebrew
+brew install \
+    awscli \
+    cue-lang/tap/cue \
+    derailed/k9s/k9s \
+    fluxcd/tap/flux \
+    gh \
+    git-secret \
+    gnupg \
+    kubectl \
+    pyenv \
+    shellcheck \
+    starship \
+    terraform \
+    terragrunt \
+    tree \
+    wget \
+    yq
+
+# Install packages using Homebrew casks
+brew install --cask \
+    docker \
+    google-chrome \
+    iterm2 \
+    visual-studio-code
 
 # Change shell to Z Shell
 chsh -s /bin/zsh
+
+# Require git commit signing
+git config --global commit.gpgsign true
 
 # Install Zsh Autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 grep 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh' ~/.zshrc || echo 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
 
-# Install Starship
-brew install starship
+# Install Starship Config
 grep 'eval "$(starship init zsh)"' ~/.zshrc || echo 'eval "$(starship init zsh)"' >> ~/.zshrc
 eval "$(starship init zsh)"
 
 # Install Powerline Fonts
 git clone https://github.com/powerline/fonts.git --depth=1
-cd fonts
-./install.sh
-cd ..
-rm -rf fonts
+eval "$(cd fonts && ./install.sh && rm -rf fonts)"
 
-# Install Github CLI
-brew install gh
-
-# Install cuelang
-brew install cue-lang/tap/cue
-
-# Install Pyenv
-brew install pyenv
+# Install Pyenv configuration and install Python 3.8.12
 grep 'export PYENV_ROOT="$HOME/.pyenv"' ~/.zshrc || echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 grep 'export PATH="$PYENV_ROOT/bin:$PATH"' ~/.zshrc || echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 grep 'eval "$(pyenv init -)"' ~/.zshrc || echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
@@ -42,40 +57,7 @@ pyenv install 3.8.12
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
-# Install AWS CLI
-brew install awscli
-
-# Install GPG
-brew install gnupg
+# Install GPG configuration
 grep 'export GPG_TTY=$(tty)' ~/.zshrc || echo 'export GPG_TTY=$(tty)' >> ~/.zshrc
-
-# Install Terraform
-brew install terraform
-
-# Install Terragrunt
-brew install terragrunt
-
-# Install Kubernetes CLI
-brew install kubectl
-
-# Install Flux CLI
-brew install fluxcd/tap/flux
-
-# Install K9s CLI
-brew install derailed/k9s/k9s
-
-# Install Visual Studio Code
-brew install --cask visual-studio-code
-
-# Install Google Chrome
-brew install --cask google-chrome
-
-# Install Docker Desktop
-brew install --cask docker
-
-# Update existing packages
-brew update
-brew upgrade
-brew upgrade --cask
 
 echo 'Done.'
