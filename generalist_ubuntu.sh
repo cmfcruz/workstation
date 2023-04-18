314988c00d27afa86a9fc23304a474e44af80470
Carlo Miguel F. Cruz <carloc@balena.io>
2023-04-18 21:19:25 +0800

    Simplify installations by using homebrew for most packages.
    Change-type: patch
    Signed-off-by: Carlo Miguel F. Cruz <cmf.cruz@gmail.com>

--- generalist_ubuntu.sh
+++ generalist_ubuntu.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Make sure .zshrc exists
+touch /home/cmfcruz/.zshrc
+
 # Install homebrew prerequisites
 sudo apt-get install -y \
     build-essential \
@@ -15,48 +18,25 @@ sudo apt-get install -y \
 
 # Install Homebrew
 test -f /usr/local/bin/brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-grep 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' || echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/cmfcruz/.zshrc
+grep 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' /home/cmfcruz/.zshrc \
+    || echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/cmfcruz/.zshrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 
 # Install packages using Homebrew
 brew install \
-    cue-lang/tap/cue \
+    balena-cli \
     derailed/k9s/k9s \
     fluxcd/tap/flux \
+    gh \
+    git-secret \
+    kubernetes-cli \
+    neovim \
+    nvm \
+    pyenv \
+    terraform \
     terragrunt \
     yq
-
-# Install git-secret
-sudo sh -c "echo 'deb https://gitsecret.jfrog.io/artifactory/git-secret-deb git-secret main' >> /etc/apt/sources.list"
-wget -qO - 'https://gitsecret.jfrog.io/artifactory/api/gpg/key/public' | sudo apt-key add -
-
-# Install Pyenv
-curl https://pyenv.run | bash
-grep "export PATH=$PATH:/home/cmfcruz/.pyenv/bin" ~/.zshrc || echo "export PATH=$PATH:/home/cmfcruz/.pyenv/bin" >> ~/.zshrc
-export PATH=$PATH:/home/cmfcruz/.pyenv/bin
-
-# Add the Kubernetes apt repository
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-
-# Install Github CLI
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-
-# Add Terraform apt repository
-wget -O- https://apt.releases.hashicorp.com/gpg | \
-    gpg --dearmor | \
-    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-gpg --no-default-keyring \
-    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
-    --fingerprint
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-    sudo tee /etc/apt/sources.list.d/hashicorp.list
-
 
 # Install packages using apt
 sudo apt update \
@@ -64,17 +44,29 @@ sudo apt update \
     docker \
     docker.io \
     docker-compose \
-    gh \
-    git-secret \
-    kubectl \
     jq \
     shellcheck \
-    terraform \
     tree \
     wget \
     vim \
-    visual-studio-code \
     zsh
+
+# Install pyenv dependencies
+sudo apt install -y \
+    build-essential \
+    libssl-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    curl \
+    libncursesw5-dev \
+    xz-utils \
+    tk-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
+    libffi-dev \
+    liblzma-dev
 
 # Change shell to Z Shell
 chsh -s /bin/zsh
@@ -91,7 +83,7 @@ git clone https://github.com/powerline/fonts.git
 cd fonts && ./install.sh && cd .. && rm -rf fonts
 
 # Install Pyenv configuration and install Python 3.8.12
-export PYTHON_VERSION=3.8.12
+export PYTHON_VERSION=3.8
 grep 'export PYENV_ROOT="$HOME/.pyenv"' ~/.zshrc || echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 grep 'export PATH="$PYENV_ROOT/bin:$PATH"' ~/.zshrc || echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 grep 'eval "$(pyenv init -)"' ~/.zshrc || echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
@@ -100,9 +92,9 @@ pyenv install $PYTHON_VERSION
 pyenv global $PYTHON_VERSION
 
 # Install nvm and install Node v16
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+grep 'export NVM_DIR="$HOME/.nvm"' ~/.zshrc || echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
+grep '[ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"' ~/.zshrc \
+    || echo '[ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"' >> ~/.zshrc
 nvm install v16
 nvm alias default v16
 
