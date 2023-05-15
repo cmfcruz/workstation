cfc967ae5382d7de11df978927e780327109af44
Carlo Miguel F. Cruz <carloc@balena.io>
2023-05-15 11:33:53 +0800

    Install nvm and pyenv using default installers.
    This avoids dependency issues that are encountered
    when installed using homebrew. nvm and pyenv are also
    now installed before homebrew.
    
    Change-type: patch
    Signed-off-by: Carlo Miguel F. Cruz <carloc@balena.io>

--- generalist_ubuntu.sh
+++ generalist_ubuntu.sh
@@ -16,43 +16,9 @@ sudo apt-get install -y \
     libssl-dev \
     software-properties-common
 
-# Install Homebrew
-test -f /usr/local/bin/brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-grep 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' /home/cmfcruz/.zshrc \
-    || echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/cmfcruz/.zshrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-
-# Install packages using Homebrew
-brew install \
-    balena-cli \
-    derailed/k9s/k9s \
-    fluxcd/tap/flux \
-    gh \
-    git-secret \
-    kubernetes-cli \
-    neovim \
-    nvm \
-    pyenv \
-    terraform \
-    terragrunt \
-    yq
-
-# Install packages using apt
+# Install pyenv dependencies
 sudo apt update \
 && sudo apt install -y \
-    docker \
-    docker.io \
-    docker-compose \
-    jq \
-    shellcheck \
-    tree \
-    wget \
-    vim \
-    zsh
-
-# Install pyenv dependencies
-sudo apt install -y \
     build-essential \
     libssl-dev \
     zlib1g-dev \
@@ -68,6 +34,57 @@ sudo apt install -y \
     libffi-dev \
     liblzma-dev
 
+# Install packages using apt
+sudo apt install -y \
+    docker \
+    docker.io \
+    docker-compose \
+    jq \
+    shellcheck \
+    tree \
+    wget \
+    vim \
+    zsh
+
+# Install Pyenv configuration and install Python 3.8.12
+curl https://pyenv.run | bash
+grep 'export PYENV_ROOT="$HOME/.pyenv"' ~/.zshrc || echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+grep 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' ~/.zshrc || echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+grep 'eval "$(pyenv init -)"' ~/.zshrc || echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+export PYTHON_VERSION=3.8
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+pyenv install $PYTHON_VERSION
+pyenv global $PYTHON_VERSION
+
+# Install nvm and install Node v16
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+grep 'export NVM_DIR="$HOME/.nvm"' ~/.zshrc || echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
+grep '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' ~/.zshrc || echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.zshrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install v16
+nvm alias default v16
+
+# Install Homebrew
+test -f /usr/local/bin/brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+grep 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' /home/cmfcruz/.zshrc \
+    || echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/cmfcruz/.zshrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# Install packages using Homebrew
+brew install \
+    balena-cli \
+    derailed/k9s/k9s \
+    fluxcd/tap/flux \
+    gh \
+    git-secret \
+    kubernetes-cli \
+    terraform \
+    terragrunt \
+    yq
+
 # Change shell to Z Shell
 chsh -s /bin/zsh
 
@@ -81,22 +98,6 @@ grep 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh' ~/.zshrc || ech
 # Install Powerline Fonts
 git clone https://github.com/powerline/fonts.git
 cd fonts && ./install.sh && cd .. && rm -rf fonts
-
-# Install Pyenv configuration and install Python 3.8.12
-export PYTHON_VERSION=3.8
-grep 'export PYENV_ROOT="$HOME/.pyenv"' ~/.zshrc || echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-grep 'export PATH="$PYENV_ROOT/bin:$PATH"' ~/.zshrc || echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-grep 'eval "$(pyenv init -)"' ~/.zshrc || echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
-eval "$(pyenv init -)"
-pyenv install $PYTHON_VERSION
-pyenv global $PYTHON_VERSION
-
-# Install nvm and install Node v16
-grep 'export NVM_DIR="$HOME/.nvm"' ~/.zshrc || echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
-grep '[ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"' ~/.zshrc \
-    || echo '[ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"' >> ~/.zshrc
-nvm install v16
-nvm alias default v16
 
 # Install GPG configuration
 grep 'export GPG_TTY=$(tty)' ~/.zshrc || echo 'export GPG_TTY=$(tty)' >> ~/.zshrc
